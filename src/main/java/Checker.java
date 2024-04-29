ca5196173e75b366941d735d9cda642c1b65392b
ycyoondev <z20152002@gmail.com>
2024-04-29 14:50:35 +0900

    refact: Checker 함수 추상화

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -5,12 +5,20 @@ public class Checker {
     public int checkLength(String inputString1, String inputString2) {
         int length1 = inputString1.length();
         int length2 = inputString2.length();
-        if (length1 == length2) {
-            return MAX_SCORE_LENGTH;
-        }
-        if (length1 >= length2 * 2 || length2 >= length1 * 2) {
-            return 0;
-        }
+        if (isMaxScore(length1, length2)) return MAX_SCORE_LENGTH;
+        if (isNoScore(length1, length2)) return 0;
+        return calcPartialScore(length1, length2);
+    }
+
+    private static int calcPartialScore(int length1, int length2) {
         return (int) ((1 - (double) Math.abs(length1 - length2) / Math.min(length1, length2)) * MAX_SCORE_LENGTH);
+    }
+
+    private static boolean isNoScore(int length1, int length2) {
+        return length1 >= length2 * 2 || length2 >= length1 * 2;
+    }
+
+    private boolean isMaxScore(int length1, int length2) {
+        return length1 == length2;
     }
 }
