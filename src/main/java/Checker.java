8b62ef4a85679efa13b158cd7ece5b912be9b3ae
ycyoondev <z20152002@gmail.com>
2024-04-29 15:07:09 +0900

    refact: PR 반영하여 함수 기능 유추 가능하도록 함수명 수정

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -6,7 +6,7 @@ public class Checker {
         int length1 = inputString1.length();
         int length2 = inputString2.length();
         if (isMaxScore(length1, length2)) return MAX_SCORE_LENGTH;
-        if (isNoScore(length1, length2)) return 0;
+        if (isZeroScore(length1, length2)) return 0;
         return calcPartialScore(length1, length2);
     }
 
@@ -14,7 +14,7 @@ public class Checker {
         return (int) ((1 - (double) Math.abs(length1 - length2) / Math.min(length1, length2)) * MAX_SCORE_LENGTH);
     }
 
-    private static boolean isNoScore(int length1, int length2) {
+    private static boolean isZeroScore(int length1, int length2) {
         return length1 >= length2 * 2 || length2 >= length1 * 2;
     }
 
