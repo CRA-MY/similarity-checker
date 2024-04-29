863aa64e00f97dd104674f1eb223b7c3a91e417c
ycyoondev <z20152002@gmail.com>
2024-04-29 14:18:09 +0900

    feat: 글자수가같은경우

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -1,2 +1,8 @@
 public class Checker {
+    public int checkLength(String inputString1, String inputString2) {
+        if (inputString1.length() == inputString2.length()) {
+            return 60;
+        }
+        return 0;
+    }
 }
--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class CheckerTest {
+    Checker checker;
 
+    @BeforeEach
+    void setUp() {
+        checker = new Checker();
+    }
+
+    @Test
+    void 글자수가같은경우() {
+        String inputString1 = "ABCD";
+        String inputString2 = "ABCE";
+        int score = checker.checkLength(inputString1, inputString2);
+        assertThat(score).isEqualTo(60);
+    }
 }
