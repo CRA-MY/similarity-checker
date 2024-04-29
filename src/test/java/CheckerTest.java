35665a8d1879945ca1f45ac31cffb71b78205da1
ycyoondev <z20152002@gmail.com>
2024-04-29 14:23:50 +0900

    refact: 테스트함수 공통화

--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -15,15 +15,18 @@ class CheckerTest {
     void 글자수가같은경우() {
         String inputString1 = "ABCD";
         String inputString2 = "ABCE";
-        int score = checker.checkLength(inputString1, inputString2);
-        assertThat(score).isEqualTo(60);
+        assertThatScoreLength(inputString1, inputString2, 60);
     }
 
     @Test
     void 글자수가두배이상차이나는경우() {
         String inputString1 = "AB";
         String inputString2 = "ABCE";
-        int score = checker.checkLength(inputString1, inputString2);
-        assertThat(score).isEqualTo(0);
+        assertThatScoreLength(inputString1, inputString2, 0);
+    }
+
+    private void assertThatScoreLength(String inputString1, String inputString2, int expectedScore) {
+        assertThat(checker.checkLength(inputString1, inputString2))
+                .isEqualTo(expectedScore);
     }
 }
